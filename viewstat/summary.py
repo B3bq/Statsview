978c5dcd9fb19562ff22e608bb0c86e4fa055f02
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2025-03-28 20:12:23 +0100

    it is almost done

--- viewstat/summary.py
+++ viewstat/summary.py
@@ -10,7 +10,7 @@ def top_leagues():
 
     mycursor = connction.cursor()
 
-    sql_query = "SELECT name FROM league ORDER BY count DESC LIMIT 5"
+    sql_query = "SELECT name, count FROM league ORDER BY count DESC LIMIT 5"
     mycursor.execute(sql_query)
     top_league_names = mycursor.fetchall()
     return top_league_names
@@ -30,3 +30,35 @@ def top_teams():
 
     top_teams_table = mycursor.fetchall()
     return top_teams_table
+
+def home_team():
+    connction = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "",
+        database = "stats"
+    )
+
+    mycursor = connction.cursor()
+
+    sql_home_team = "SELECT name, homeCount FROM teams ORDER BY homeCount DESC LIMIT 1"
+    mycursor.execute(sql_home_team)
+
+    top_home_team = mycursor.fetchall()
+    return top_home_team
+
+def away_team():
+    connction = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "",
+        database = "stats"
+    )
+
+    mycursor = connction.cursor()
+
+    sql_home_team = "SELECT name, awayCount FROM teams ORDER BY awayCount DESC LIMIT 1"
+    mycursor.execute(sql_home_team)
+
+    top_home_team = mycursor.fetchall()
+    return top_home_team
--- viewstat/summary_window.py
+++ viewstat/summary_window.py
@@ -17,21 +17,34 @@ class Summary(QWidget):
     def setup(self):
 
         league_names = top_leagues()
-        league_names = [name[0] for name in league_names]
         y = 100
-        for i in range(0, len(league_names)):
-            self.label_league = QLabel(f"{i+1}. {league_names[i]}", self)
+        i = 1
+        for name, count in league_names:
+            self.label_league = QLabel(f"{i}. {name} {count}", self)
             self.label_league.move(200 , y)
             y+=25
+            i+=1
 
         teams_table = top_teams()
-        teams_table = [name[0] for name in teams_table]
         y = 100
-        for i in range(0, len(teams_table)):
-            self.label_teams = QLabel(f"{i+1}. {teams_table[i]}", self)
+        i = 1
+        for name, count in teams_table:
+            self.label_teams = QLabel(f"{i}. {name} {count}", self)
             self.label_teams.move(800, y)
             y+=25
+            i+=1
             
+        top_home = home_team()
+
+        for name, count in top_home:
+            self.label_top_home = QLabel(f"{name} {count}",self)
+            self.label_top_home.move(450, 150)
+
+        top_away = away_team()
+
+        for name, count in top_away:
+            self.label_top_away = QLabel(f"{name} {count}",self)
+            self.label_top_away.move(450, 250)
 
         # back to menu button
         back_btn = QPushButton("Back", self)
