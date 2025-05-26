35bd333adbf966a4ddb259a50636a48968e71398
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2025-05-26 22:34:37 +0200

    simple palette and sign in design

--- website/src/js/script.js
+++ website/src/js/script.js
@@ -1,3 +1,4 @@
+// toggle menu script
 const toggleMenuEl = document.getElementById('js-toggle-menu');
 const toggleableMenuEl = document.getElementById('js-toggleable-menu');
 
@@ -8,4 +9,13 @@ toggleMenuEl?.addEventListener('click', function(){
 const breakpoint = window.matchMedia('(min-width: 768px)');
 breakpoint.addEventListener('change', function(){
     toggleableMenuEl?.classList.remove('active');
+})
+
+// script to show password
+const pass = document.getElementById('password');
+const re_pass = document.getElementById('re_password');
+const show_pass = document.getElementById('show_password');
+
+show_pass?.addEventListener('checked', function(){
+    pass?.classList.value('show')
 })
