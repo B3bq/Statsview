14a8f47ff24d6f271f8a9fb26b22a72959a1d9c5
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2025-05-30 20:10:09 +0200

    some changes

--- website/src/js/script.js
+++ website/src/js/script.js
@@ -16,6 +16,7 @@ const pass = document.getElementById('password');
 const re_pass = document.getElementById('re_password');
 const show_pass = document.getElementById('show_password');
 
-show_pass?.addEventListener('checked', function(){
-    pass?.classList.value('show')
-})
+show_pass.addEventListener('change', function(){
+    pass.type = this.checked ? 'text' : 'password';
+    re_pass.type = this.checked ? 'text' : 'password';
+});
