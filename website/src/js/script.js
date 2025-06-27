33e64964819e4e14eb3203209095c5690d7a47f0
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2025-06-27 21:50:24 +0200

    some changes

--- website/src/js/script.js
+++ website/src/js/script.js
@@ -1,9 +1,18 @@
 // toggle menu script
 const toggleMenuEl = document.getElementById('js-toggle-menu');
 const toggleableMenuEl = document.getElementById('js-toggleable-menu');
+const menu = document.getElementById('menu');
+let isOpen = false;
 
 toggleMenuEl?.addEventListener('click', function(){
     toggleableMenuEl?.classList.toggle('active');
+    if(isOpen){
+        menu.src = "src/img/arrow.png";
+    }
+    else{
+        menu.src = "src/img/burger-bar.png";
+    }
+    isOpen = !isOpen; // change condition
 })
 
 const breakpoint = window.matchMedia('(min-width: 768px)');
