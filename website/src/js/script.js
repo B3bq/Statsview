f11bc420513d32bba40f5171d42198ec8dae4fd1
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2025-07-20 17:18:36 +0200

    taking summary depending on which season is

--- website/src/js/script.js
+++ website/src/js/script.js
@@ -94,12 +94,33 @@ window.addEventListener('DOMContentLoaded', ()=>{
 })
 
 //SUMMARY
+//summary text
+window.addEventListener('DOMContentLoaded', ()=>{
+    //taking season from url
+    let querystring = window.location.search;
+    let urlParam = new URLSearchParams(querystring);
+    let season = urlParam.get('season');
+
+    if(season == 'year'){
+        document.getElementById('lol').hidden =false;
+        document.getElementById('cs').hidden =false;
+    }else{
+        document.getElementById('lol').hidden =true;
+        document.getElementById('cs').hidden =true;
+    }
+
+    document.getElementById('text').innerHTML = "Check your " + season + " summary";
+})
+
 //selecting sport
 document.getElementById('sport').addEventListener("change", function(){
     //taking season from url
     let querystring = window.location.search;
     let urlParam = new URLSearchParams(querystring);
     let season = urlParam.get('season');
+
+    document.getElementById('text').style.visibility = 'hidden';
+
     const selected = this.value; //take selected value
 
     fetch('src/php/summary.php', {
@@ -111,8 +132,11 @@ document.getElementById('sport').addEventListener("change", function(){
     })
     .then(response=>response.json())
     .then(data=>{
-        console.log(data);
-        document.getElementById('data').innerHTML = data.summary + data.home + data.away + data.leagues + data.teams;
+        if(data.home && data.away){
+            document.getElementById('data').innerHTML = data.summary + data.home + data.away + data.leagues + data.teams;
+        }else{
+            document.getElementById('data').innerHTML = data.summary + data.leagues + data.teams;
+        }
     })
 })
 
--- website/src/php/summary.php
+++ website/src/php/summary.php
@@ -12,6 +12,109 @@ $season = $_POST['season'];
 $sport = $_POST['sport'];
 
 if($season == 'season'){
+    $tableLeague = $sport."_leagues_".$season;
+    $tableTeams = $sport."_teams_".$season;
+    //overall matches
+    $sql = "SELECT SUM(count) FROM $tableLeague WHERE id_user = ?";
+    $query = $conn->prepare($sql);
+    $query->bind_param("s", $userID);
+    $query->execute();
+    $result = $query->get_result();
+    $ovrCount = $result->fetch_assoc()['SUM(count)'];
+
+    $gamesText = "<h1>You watched $ovrCount games!</h1>"; //info to website
+
+    //top 5 leagues
+    $sql = "SELECT $tableLeague.name, count, images.img FROM $tableLeague JOIN images ON $tableLeague.img = images.id WHERE id_user = $userID ORDER BY count DESC LIMIT 5";
+    $topLeagus = mysqli_query($conn, $sql);
+
+    //table 
+    $leagueTable = "<table class='topLeagues'>";
+    $leagueTable .= "<tr><th>on.</th><th colspan='2'>Name</th><th>Matches</th></tr>";
+
+    $n = 1;
+
+    while($row=mysqli_fetch_assoc($topLeagus)){
+        $finfo = new finfo(FILEINFO_MIME_TYPE);
+        $mimeType = $finfo->buffer($row['img']); //taikng file type
+        $base64 = 'data:'.$mimeType.';base64,'.base64_encode($row['img']); //code this to base64
+        $leagueTable .= "<tr><td>".$n."</td><td>".$row['name']."</td><td><img src='".$base64."'></td><td>".$row['count']."</td></tr>";
+        $n++;
+    }
+    $leagueTable .= "</table>";
+
+    //top 5 teams
+    $sql = "SELECT $tableTeams.name, (home_count + away_count) AS total_count, images.img FROM $tableTeams JOIN images ON $tableTeams.img = images.id WHERE id_user = $userID ORDER BY total_count DESC LIMIT 5";
+    $topTeams = mysqli_query($conn, $sql);
+
+    //tabel
+    $teamTable = "<table class='topTeams'>";
+    $teamTable .= "<tr><th>on.</th><th colspan='2'>Name</th><th>Matches</th></tr>";
+
+    $n = 1;
+
+    while($row=mysqli_fetch_assoc($topTeams)){
+        $finfo = new finfo(FILEINFO_MIME_TYPE);
+        $mimeType = $finfo->buffer($row['img']);
+        $base64 = 'data:'.$mimeType.';base64,'.base64_encode($row['img']);
+        $teamTable .= "<tr><td>".$n."</td><td>".$row['name']."</td><td><img src='".$base64."'></td><td>".$row['total_count']."</td></tr>";
+        $n++;
+    }
+    $teamTable .= "</table>";
+
+    //top home team
+    $sql = "SELECT $tableTeams.name, home_count, images.img FROM $tableTeams JOIN images ON $tableTeams.img = images.id WHERE id_user = $userID ORDER BY home_count DESC LIMIT 1";
+    $result = mysqli_query($conn, $sql);
+    $topHome = mysqli_fetch_assoc($result);
+
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    $mimeType = $finfo->buffer($topHome['img']); //taikng file type
+    $base64 = 'data:'.$mimeType.';base64,'.base64_encode($topHome['img']); //code this to base64
+
+    $homeFav = "<div class='fav'><h2>Home favourite</h2><img src='$base64'><h3>".$topHome['name']." (".$topHome['home_count']." matches)</h3></div>";
+
+    //top away team
+    $sql = "SELECT $tableTeams.name, away_count, images.img FROM $tableTeams JOIN images ON $tableTeams.img = images.id WHERE id_user = $userID ORDER BY away_count DESC LIMIT 1";
+    $result = mysqli_query($conn, $sql);
+    $topAway = mysqli_fetch_assoc($result);
+
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    $mimeType = $finfo->buffer($topAway['img']); //taikng file type
+    $base64 = 'data:'.$mimeType.';base64,'.base64_encode($topAway['img']); //code this to base64
+
+    $awayFav = "<div class='fav'><h2>You traveled with them most often</h2><img src='$base64'><h3>".$topAway['name']." (".$topAway['away_count']." matches)</h3></div>";
+
+    //pack datas for json
+    $data = [
+        'summary' => $gamesText,
+        'home' => $homeFav,
+        'away' => $awayFav,
+        'leagues' => $leagueTable,
+        'teams' => $teamTable,
+    ];
+
+    //function to standardized coding
+    function utf8ize($data) {
+        if (is_array($data)) {
+            foreach ($data as $key => $value) {
+                $data[$key] = utf8ize($value);
+            }
+        } elseif (is_string($data)) {
+            return mb_convert_encoding($data, "UTF-8", "auto");
+        }
+        return $data;
+    }
+    
+    $data = utf8ize($data);
+
+    header('Content-Type: application/json'); //answer type
+    $json =  json_encode($data);
+    if($json === false){ //checking
+        echo json_last_error_msg();
+    }else{
+        echo $json;
+    }
+}else{
     switch ($sport){
         case "lol":
             //overall matches
@@ -277,7 +380,5 @@ if($season == 'season'){
 
             break;
     }
-}else{
-    
 }
 ?>
