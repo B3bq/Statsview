8b4048c732622305122ffcdf32e6fe0ef4149570
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2025-07-27 16:55:15 +0200

    stylization for tablet

--- website/src/js/script.js
+++ website/src/js/script.js
@@ -38,7 +38,7 @@ function deleteCookie(name) {
 function visibleSummary(){
     // list of allowed dates in "MM-DD" format
     const allowedDates = [
-        "07-24",
+        "07-27",
         "07-15",
         "07-16",
         "07-17",
